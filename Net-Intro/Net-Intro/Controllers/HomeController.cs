480136d3f41d19bbfa63a4bf51a987e052c64d7f
metlebh <matlabyh@code.edu.az>
2023-05-25 15:33:09 +0400

    All done

---
+++ Net-Intro/Net-Intro/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Net_Intro.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Name = "Metleb";
+            ViewBag.Surname = "Huseynov";
+            ViewBag.Age = 25;
+            ViewData["Country"] = "Azerbaijan";
+            ViewData["Phone Number"] = "+994993991019";
+            return View();
+
+
+        }
+    }
+}
